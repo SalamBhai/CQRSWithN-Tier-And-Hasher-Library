f933665e07e0074e42e6cea56b25b7e55c88fcfe
Salam Bhai <94355563+SalamBhai@users.noreply.github.com>
2022-09-14 22:39:16 +0100

    Delete IHasher.cs

--- NubSkull/Authentication/Hasher/IHasher.cs
+++
@@ -1,7 +0,0 @@
-namespace NubSkull.Authentication.Hasher;
-
-public interface IHasher
-{
-    string GenerateHash(string Password);
-    bool ValidatePassword(string SavedPassword, string Password);
-}
